b2a9fbdeea2c03ba430877e1b16464b707eb9883
Renan Carneiro <carneiro.renan5@gmail.com>
2022-03-05 20:10:43 -0300

    Create 1050-ddd.py

---
+++ 1050-ddd.py
@@ -0,0 +1,22 @@
+ddd = int(input())
+codigos = [61,71,11,21,32,19,27,31]
+if ddd == 61:
+    print("Brasilia")
+elif ddd == 71:
+    print("Salvador")
+elif ddd == 11:
+    print("Sao Paulo")
+elif ddd == 21:
+    print("Rio de Janeiro")
+elif ddd == 32:
+    print("Juiz de Fora")
+elif ddd == 19:
+    print("Campinas")
+elif ddd == 27:
+    print("Vitoria")
+elif ddd == 31:
+    print("Belo Horizonte")
+
+elif ddd != codigos:
+    print("DDD nao cadastrado")
+  
