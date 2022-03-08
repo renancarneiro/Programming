45b73ec4c17311989f357b92c73e0bdaa807bbb1
Renan Carneiro <carneiro.renan5@gmail.com>
2022-03-08 18:07:51 -0300

    Create m_cm_mm.py

---
+++ Curso Exercicios/m_cm_mm.py
@@ -0,0 +1,6 @@
+#Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
+# 1 metro = 100 centimetros = 1000 milimetros
+n = int(input())
+centimetros = n * 100
+milimetros = n * 1000
+print(f'{n}m = {centimetros}cm = {milimetros}mm')
