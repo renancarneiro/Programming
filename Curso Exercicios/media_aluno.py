3c0c1392e6cfe8d0d94e17e1f3b2aad391134947
Renan Carneiro <carneiro.renan5@gmail.com>
2022-03-08 17:55:20 -0300

    Create media_aluno.py

---
+++ Curso Exercicios/media_aluno.py
@@ -0,0 +1,5 @@
+#Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+nota1 = float(input())
+nota2 = float(input())
+media = (nota1 + nota2) / 2
+print(f'A média do aluno é {media}')
