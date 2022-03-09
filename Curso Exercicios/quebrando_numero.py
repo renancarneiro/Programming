40c694d462b8ecfb84c41da2acd8a8763f33ec7e
Renan Carneiro <carneiro.renan5@gmail.com>
2022-03-09 16:42:57 -0300

    Create quebrando_numero.py

---
+++ Curso Exercicios/quebrando_numero.py
@@ -0,0 +1,6 @@
+#Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira
+import math
+n = float(input())
+n = int(n)
+print(n)
+print('Outra forma',math.trunc(n))
