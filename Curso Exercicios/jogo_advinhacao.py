5255f4c635ecae19bf84218f9dd0d2291a28c78b
Renan Carneiro <carneiro.renan5@gmail.com>
2022-03-14 14:08:10 -0300

    Create jogo_advinhacao.py

---
+++ Curso Exercicios/jogo_advinhacao.py
@@ -0,0 +1,19 @@
+#Melhore o jogo do DESAFIO 28 onde o computador vai “pensar” em um número entre 0 e 10. 
+#Só que agora o jogador vai tentar adivinhar até acertar.
+#mostrando no final quantos palpites foram necessários para vencer.
+import random
+from time import sleep
+computador = random.randrange(1,11)
+jogador = int(input('Digite um numero de 1 a 10: '))
+contador = 0
+
+while jogador != computador:
+    jogador = int(input(f'Você errou! Tente novamente\nO computador escolheu o numero {computador}'))
+    contador+=1
+    computador = random.randrange(1,10)
+    
+if contador == 0:
+    print(f'Você acertou de primeira, parabens!\nO computador escolheu {computador} também')
+else:
+    print(f'Você acertou, o computador escolheu {computador} também, mas demorou {contador} vezes até acertar')
+    
