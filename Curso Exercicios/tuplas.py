2c27a1ff934e593441731340e857325a2ffa65a9
Renan Carneiro <carneiro.renan5@gmail.com>
2022-03-18 20:47:27 -0300

    Create tuplas.py

---
+++ Curso Exercicios/tuplas.py
@@ -0,0 +1,11 @@
+lanche = ('pizza','hamburguer','suco','bolo')
+sucos = ('cupuaçu','goiaba','acerola', 'pizza')
+juntar = lanche + sucos
+#print(sorted(juntar))
+print(juntar)
+print(juntar.count('pizza'))
+print(juntar.index('goiaba'))
+#print(sorted(lanche))
+#for pos, x in enumerate(lanche):
+#    print(f'Eu vou comer {x} | está na posição {pos}')
+#del(tuplas)
