225416f8131c774e064254790276beb3cf984911
Renan Carneiro <carneiro.renan5@gmail.com>
2022-03-18 21:04:13 -0300

    Create numero_por_extenso.py

---
+++ Curso Exercicios/numero_por_extenso.py
@@ -0,0 +1,7 @@
+#Crie um programa que tenha uma dupla totalmente preenchida com uma contagem por extenso, de zero até vinte. 
+#Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
+numeros = ('zero','um','dois','três','quatro','cinco','seis','sete','oito','nove','dez','onze','doze','treze','quatorze','quinze','dezesseis','dezessete','dezoito','dezenove','vinte')
+n = int(input('Digite um numero de 0 a 20'))
+while n > 20:
+    n = int(input('Tente novamente! Digite um numero entre 0 e 20'))
+print(f'Você digitou o numero {numeros[n]}')
