1e1d225e791c8eb6c828bcc39ca93ff92e837136
Renan Carneiro <carneiro.renan5@gmail.com>
2022-03-20 15:27:29 -0300

    Create lista_ordenada_sem_sort_v2.py

---
+++ Curso Exercicios/lista_ordenada_sem_sort_v2.py
@@ -0,0 +1,16 @@
+#Crie um programa onde o usuário possa digitar cinco valores numéricos e cadastre-os em uma lista, 
+#já na posição correta de inserção (sem usar o sort()). No final, mostre a lista ordenada na tela.
+lista = []
+for x in range(0,5):
+    n = int(input('Digite um valor'))
+    if x == 0 or n > lista[-1]:
+        lista.append(n)
+        print('Adicionado na última posição')
+    else:
+        pos = 0
+        while pos < len(lista):
+            if n <= lista[pos]:
+                lista.insert(pos, n)
+                print(f'Adicionado {n} na posição {pos}')
+                break
+print(f'A lista em ordem: {lista}')
