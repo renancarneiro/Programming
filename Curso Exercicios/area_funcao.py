4350ee7a76dd7d01474c70544b86df431a8a46b5
Renan Carneiro <carneiro.renan5@gmail.com>
2022-03-26 14:44:53 -0300

    Create area_funcao.py

---
+++ Curso Exercicios/area_funcao.py
@@ -0,0 +1,8 @@
+def area(larg,comp):
+    area = larg * comp
+    print(f'A area de um terreno de {larg}m² x {comp}² é = {area}m²')
+
+
+largura = float(input('Largura da area:'))
+comprimento = float(input('Comprimento da area:'))
+area(largura,comprimento)
