f6bbee98d2e759adc356c3b708f277b88469cd8c
Renan Carneiro <carneiro.renan5@gmail.com>
2022-03-31 15:07:15 -0300

    Create site_acessivel.py

---
+++ Curso Exercicios/site_acessivel.py
@@ -0,0 +1,10 @@
+import urllib
+import urllib.request
+
+try:
+    site = urllib.request.urlopen('http://www.pudim.com.br')
+except urllib.error.URLError:
+    print('O site não está acessivel')
+else:
+    print('Site acessado com suceso')
+    
